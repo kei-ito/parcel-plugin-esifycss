56f6c3b9e3ac0e14385be2a8e49221412e5fa04d
Kei Ito <kei.itof@gmail.com>
2019-07-08 15:37:19 +0900

    fix: modify module.exports directly

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
-import Bundler from 'parcel-bundler';
-import {setDictionary} from './setDictionary';
-
-const plugin = (
-    bundler: Bundler,
-) => {
-    bundler.addAssetType('css', require.resolve('./CSSAsset'));
-    bundler.addAssetType('esifycss', require.resolve('./HelperAsset'));
-    bundler.on('bundled', setDictionary);
-};
-
-export default plugin;
+import {plugin} from './plugin';
+module.exports = Object.assign(plugin, {plugin});
